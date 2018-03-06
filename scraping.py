f1d1274c2db36fe9161dfe6aae763654041ab5bd
Benjamin Greene <b.greene@wustl.edu>
2018-03-06 12:56:37 -0500

    Flask setup, initial scraping

---
+++ scraping.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+def scrape():
+	data = []
+	url_main = "http://eltiempo.com"
+
+	r = requests.get(url_main)
+	soup = BeautifulSoup(r.text, "html.parser")
+	articles = soup.find_all("h3", class_="listing-title")
+
+	for article in articles:
+		info = {}
+		link = article.find("a")
+		#title
+		info["title"] = link.text
+
+		#url
+		info['url'] = "http://eltiempo.com" + link['href']
+		
+		data.append(info)
+
+	return data
+
+
+		
